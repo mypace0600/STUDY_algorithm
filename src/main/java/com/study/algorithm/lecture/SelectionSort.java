471969e2cfa7c067fc072b225b7e33ea1949668a
mypace0600 <mypace0600@gmail.com>
2024-05-26 17:41:15 +0900

    array로 명칭 변경

--- src/main/java/com/study/algorithm/lecture/SelectionSort.java
+++ src/main/java/com/study/algorithm/lecture/SelectionSort.java
@@ -24,12 +24,12 @@ public class SelectionSort {
         }
     }
 
-    private int[] solution(int[] randomNumbers){
+    private int[] solution(int[] array){
         /*
         선택정렬 : 가장 작은 것을 선택해서 제일 앞으로 보내면 어떨까?
         */
 
-        int size = randomNumbers.length;
+        int size = array.length;
         int i=0;
         int j=0;
         int index =0;
@@ -38,16 +38,16 @@ public class SelectionSort {
         for(i=0;i<size;i++){
             int min=10000;
             for(j=i;j<size;j++){
-                if(min>randomNumbers[j]){
-                    min = randomNumbers[j];
+                if(min>array[j]){
+                    min = array[j];
                     index = j;
                 }
             }
-            temp = randomNumbers[i];
-            randomNumbers[i] = randomNumbers[index];
-            randomNumbers[index] = temp;
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
         }
 
-        return randomNumbers;
+        return array;
     }
 }
